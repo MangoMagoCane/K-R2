921d6f072fef1fb4e69acb495d506daa3f9e8184
MangoMagoCane <sir.mangocane@gmail.com>
2024-03-05 09:03:34 -0500

    Add getword to getchars.h and modify 6-1 to use pointers

--- headers/getchars.h
+++ headers/getchars.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 #define BUFSIZE 1000
 
@@ -19,4 +21,28 @@ void ungetch(int c)
     } else {
         buf[bufp++] = c;
     }
+}
+
+/* getword: get next word or character from input */
+int getword(char *word, int lim)
+{
+    int c, getch(void);
+    void ungetch(int);
+    char *w = word;
+    while (isspace(c = getch())) {}
+    if (c != EOF) {
+        *w++ = c;
+    }
+    if (!isalpha(c)) {
+        *w = '\0';
+        return c;
+    }
+    for ( ; --lim > 0; w++) {
+        if (!isalnum(*w = getch())) {
+            ungetch(*w);
+            break;
+        }
+    }
+    *w = '\0';
+    return word[0];
 }
