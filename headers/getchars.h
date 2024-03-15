18b208f7dfc93426d6ad73e4c90eb89eeb39dfa3
Val Garman <sir.mangocane@gmail.com>
2024-03-15 00:24:56 -0400

    Reorganize source code

--- headers/getchars.h
+++ headers/getchars.h
@@ -29,6 +29,7 @@ int getword(char *word, int lim)
     int c, getch(void);
     void ungetch(int);
     char *w = word;
+
     while (isspace(c = getch())) {}
     if (c != EOF) {
         *w++ = c;
@@ -37,12 +38,14 @@ int getword(char *word, int lim)
         *w = '\0';
         return c;
     }
-    for ( ; --lim > 0; w++) {
+
+    for (; --lim > 0; w++) {
         if (!isalnum(*w = getch())) {
             ungetch(*w);
             break;
         }
     }
+    
     *w = '\0';
     return word[0];
 }
