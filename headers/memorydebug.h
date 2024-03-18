096eb49b90a8bc063755aa956edd2a2bb644d51f
MangoMagoCane <sir.mangocane@gmail.com>
2024-03-18 14:08:06 -0400

    feat: Add functionality for strdup()

--- headers/memorydebug.h
+++ headers/memorydebug.h
@@ -1,20 +1,27 @@
 #include <stdlib.h>
+#include <string.h>
 
-static int malfree = 0;
+static int __malfree__ = 0;
 
 void *my_malloc(size_t size)
 {
     void *ptr = malloc(size);
-    malfree++;
+    __malfree__++;
     return ptr;
 }
 
 void my_free(void *ptr)
 {
-    malfree--;
+    __malfree__--;
     free(ptr);
+}
+
+char *my_strdup(char *s) {
+    __malfree__++;
+    return strdup(s);
 }
 
 #define malloc(x) my_malloc(x)
 #define free(x) my_free(x)
-#define mallocsfreed() printf("Unfreed mallocs: %d\n", malfree);
+#define strdup(x) my_strdup(x)
+#define mallocsfreed() printf("Unfreed mallocs: %d\n", __malfree__);
